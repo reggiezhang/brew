941b7cdc8df6ac78b9942834c5114184fa72fbaa
Reggie Zhang <reggiezhang@users.noreply.github.com>
2018-05-20 16:35:21 +0800

    Update ss-verbose.rb

--- ss-verbose.rb
+++ ss-verbose.rb
@@ -41,7 +41,7 @@ class SsVerbose < Formula
 
   plist_options :manual => "#{HOMEBREW_PREFIX}/opt/shadowsocks-libev/bin/ss-local -c #{HOMEBREW_PREFIX}/etc/shadowsocks-libev.json"
 
-  def plist; <<-EOS.undent
+  def plist; <<~EOS
     <?xml version="1.0" encoding="UTF-8"?>
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
